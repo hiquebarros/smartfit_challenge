2f98540b7d84f8f227393835e471aaff0661293f
Henrique Barros <henrique1_barros@hotmail.com>
2022-11-08 16:53:07 -0300

    fix: paragraph text rendering

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,62 +10,74 @@ import required_mask from "../../assets/icons/required-mask.png"
 import required_towel from "../../assets/icons/required-towel.png"
 
 interface ISchedule {
-    weekdays: string
-    hour: string
-  }
-  
-  interface ILocation {
-    content: string
-    id: number
-    locker_room: string
-    mask: string
-    opened: boolean
-    schedules: Array<ISchedule>
-    title: string
-    towel: string
-    fountain: string
+  weekdays: string
+  hour: string
+}
+
+interface ILocation {
+  content: string
+  id: number
+  locker_room: string
+  mask: string
+  opened: boolean
+  schedules: Array<ISchedule>
+  title: string
+  towel: string
+  fountain: string
+}
+
+interface ICardProps {
+  data: ILocation
+}
+
+const formatParagraph = (data: string) => {
+
+  let arr = data.split(";")
+  let newArr = arr.map(item => {
+    let pFormated = item.replace("<p>", "").replace("</p>", "").replace("<p>", "").replace("</p>", "")
+    let strongFormated = pFormated.replace("<strong>", "").replace("</strong>", "")
+    let brFormated = strongFormated.replace("<br>", " ").replace("</br>", "")
+    let spanFormated = brFormated.replace("<span>", "").replace("</span>", "")
+    let index = spanFormated.indexOf('&')
+    let hashFormated = spanFormated.slice(0, index)
+    return hashFormated
+  })
+
+  return newArr.join()
+}
+
+const Card = ({ data }: ICardProps) => {
+
+  if (data.title === "Presidente Costa e Silva") {
+    console.log(data)
   }
 
-  interface ICardProps {
-    data: ILocation
-  }
+  return (
+    <Container open={data.opened}>
+      <div className="card-header">
+        {data.opened ? (<h5>Aberto</h5>) : (<h5>Fechado</h5>)}
+        <h2>{data.title}</h2>
+        <p>{formatParagraph(data.content)}</p>
+      </div>
+      <div className="card-icons">
+        {data.mask == "required" ? (<img src={required_mask} />) : (<img src={recommended_mask} />)}
+        {data.towel == "required" ? (<img src={required_towel} />) : (<img src={recommended_towel} />)}
+        {data.fountain == "partial" ? (<img src={partial_fountain} />) : (<img src={forbidden_fountain} />)}
+        {data.locker_room == "allowed" ? (<img src={required_lockerroom} />) : data.locker_room == "partial" ? (<img src={partial_lockerroom} />) : (<img src={forbidden_lockerroom} />)}
+      </div>
+      <div className="card-schedules">
 
-  const formatParagraph = (data: string) => {
-    let stringArray = data.slice(4, -5).split(";")
-    let arr1 = stringArray[0].slice(0, -7) + ";"
-    let arr2 = stringArray[1].replace("<br>", " ") + "."
-
-    return arr1 + arr2
-  }
-
-const Card = ({data}: ICardProps) => {
-    
-    return (
-        <Container open={data.opened}>
-            <div className="card-header">
-              {data.opened ? (<h5>Aberto</h5>) : (<h5>Fechado</h5>)}
-                <h2>{data.title}</h2>
-                <p>{formatParagraph(data.content)}</p>
-            </div>
-            <div className="card-icons">
-            {data.mask == "required" ? (<img src={required_mask}/>) : (<img src={recommended_mask}/>)}
-            {data.towel == "required" ? (<img src={required_towel}/>) : (<img src={recommended_towel}/>)}
-            {data.fountain == "partial" ? (<img src={partial_fountain}/>) : (<img src={forbidden_fountain}/>)}
-            {data.locker_room == "allowed" ? (<img src={required_lockerroom}/>) : data.locker_room == "partial" ? (<img src={partial_lockerroom}/>) : (<img src={forbidden_lockerroom}/>)}
-            </div>
-            <div className="card-schedules">
-              
-              {!!data.schedules && data.schedules.map(item => {
-                return (
-                <CardSchedule key={Math.random()}>
-                  <h3>{item.weekdays}</h3>
-                  <h5>{item.hour}</h5>
-                </CardSchedule>
-                )
-              })}
-            </div>
-        </Container>
-    );
+        {!!data.schedules && data.schedules.map(item => {
+          return (
+            <CardSchedule key={Math.random()}>
+              <h3>{item.weekdays}</h3>
+              <h5>{item.hour}</h5>
+            </CardSchedule>
+          )
+        })}
+      </div>
+    </Container>
+  );
 }
 
 export default Card;
